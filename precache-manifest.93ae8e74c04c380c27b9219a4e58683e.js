54a8d42be7616cf5ae2236d2fbfe01ebfb87a7e7
Chaitanya Deshpande <cdeshpa2@gmail.com>
2020-09-03 00:37:34 -0400

    Updates

---
+++ precache-manifest.93ae8e74c04c380c27b9219a4e58683e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fccbf2027c7c45bda9f443bdf774dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "265e6973ef56977494d6",
+    "url": "/monsters-rolodex/static/css/main.9692886a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265e6973ef56977494d6",
+    "url": "/monsters-rolodex/static/js/main.89170287.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
